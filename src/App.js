07fbcadaebcd4b9e22100b11f97e966e35b7b61a
Paul Salamone <93609282+paulsalamone@users.noreply.github.com>
2022-07-11 15:11:08 +0200

    first commit of rebuilt version

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ function App() {
   return (
     <div className="app">
       <header>
-        <h1>Illuminated CSS</h1>
+        <Link to="/">
+          {" "}
+          <h1 class="logo slide">Illuminated CSS</h1>
+        </Link>
+
         <p>
           Illustrated Quotes for practicing CSS styling. RULE: no images! Pure
           CSS! Allowed: imported fonts, native SVG, animation.
         </p>
         <nav>
           <ul>
-            <Link to="/">Home</Link>
             <Link to="/quotes/Q001">quote 1</Link>
             <Link to="/quotes/Q002">quote 2</Link>
           </ul>
